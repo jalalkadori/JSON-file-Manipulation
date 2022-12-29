8253d62fd8ef3769af2db741d93a79042725c9fb
Jalal KADDOURI <jalalkadori2@gmail.com>
2022-12-29 23:12:19 +0100

    updating script

--- main.js
+++ main.js
@@ -1,25 +1,19 @@
 // Create an XMLHttpRequest object:
 var xhr = new XMLHttpRequest();
+let table = document.getElementById('table');
+let row = document.createElement('tr');
+let cell = document.createElement('td');
+let output = document.getElementById('output');
+
 // Open a connection to the JSON file using the open() method:
 xhr.open('GET', 'movies.json', true);
 xhr.responseType = 'json';
 xhr.send();
 xhr.addEventListener('load', function() {
-    var data = xhr.response;
-    console.log(data)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    var Movies_data = xhr.response;
+    let moviesList = Movies_data ;
+    console.log(moviesList);
+    console.log(moviesList.movies[0].festivals[1]);
   });
+
 
